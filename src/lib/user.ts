e875aef587cba8dbb00c208303c65031853f23a2
ShaddyDC <shaddythefirst@gmail.com>
2022-06-06 16:42:45 +0200

    Switch to cookie-based authentication

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,5 +1,4 @@
 import type { CredentialResponse } from 'google-one-tap';
-import Cookies from 'js-cookie';
 import { derived, writable, type Writable } from 'svelte/store';
 
 interface User {
@@ -10,28 +9,31 @@ const userStore: Writable<User | undefined> = writable(undefined);
 export const user = derived(userStore, ($userStore) => $userStore);
 
 export async function check_login() {
-  const session = Cookies.get('session');
-  const request = await fetch('http://localhost:8000/api/v1/check_login', {
-    method: 'POST',
+  const request = await fetch('https://localhost/api/v1/check_login', {
+    method: 'GET',
     mode: 'cors',
-    body: session
+    credentials: 'same-origin'
   });
   const logged_in = request.status === 200;
 
-  const user = logged_in ? await request.json() : undefined;
+  const json = await request.json();
+
+  console.log('check response', request, json);
+
+  const user = logged_in ? json : undefined;
   userStore.update(() => user);
 
   return user;
 }
 
 export async function handleCredentialResponse({ credential }: CredentialResponse) {
-  const response = await fetch('http://localhost:8000/api/v1/login', {
+  const response = await fetch('https://localhost/api/v1/login', {
     method: 'POST',
     mode: 'cors',
+    credentials: 'same-origin',
     body: credential
   });
-  const session = await response.text();
-  Cookies.set('session', session);
+  console.log('login response', response);
 
   return await check_login();
 }
